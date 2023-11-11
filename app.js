0d214c606098db6c448ad9167e301a1faace9069
kabita164 <kabita164@gmail.com>
2023-11-11 11:54:18 +0000

    sort criteria and order functionalities added

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var webstore = new Vue({
     siteName: "After School Lesson",
     showProducts: true,
     products: lessons,
+    sortCriteria: "subject", // default sort criteria
+    sortOrder: "asc", // default sort order
     cart: [],
   },
   methods: {
@@ -27,8 +29,37 @@ var webstore = new Vue({
     showCheckout() {
       this.showProducts = this.showProducts ? false : true;
     },
+    // method to set the sorting criteria
+    setSortCriteria(criteria) {
+      this.sortCriteria = criteria;
+    },
+    // method to toggle sorting order between 'asc' and 'desc'
+    toggleSortOrder(order) {
+      this.sortOrder = order;
+    },
   },
   computed: {
+    sortedLessons() {
+      const criteria = this.sortCriteria;
+      const order = this.sortOrder === "asc" ? 1 : -1;
+
+      let lessons = this.products.slice(); // create a copy of the array
+
+      return lessons.sort((a, b) => {
+        // If sorting by remaining spaces, compare by (spaces - cartCount)
+        if (this.sortCriteria === "spaces") {
+          const remainingSpacesA = a.spaces - this.cartCount(a.id);
+          const remainingSpacesB = b.spaces - this.cartCount(b.id);
+
+          return (remainingSpacesA - remainingSpacesB) * order;
+        }
+
+        // For oher sorting criteria
+        if (a[criteria] < b[criteria]) return -order;
+        if (a[criteria] > b[criteria]) return order;
+        return 0;
+      });
+    },
     cartItemCount: function () {
       return this.cart.length || "";
     },
